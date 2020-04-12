76c9d0d550584143d2169e928b440a984ab26140
stosifian <rxastosifian@gmail.com>
2020-04-12 15:40:46 -0700

    django stuff

---
+++ NowChatDJ/NowChatDJ/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for NowChatDJ project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NowChatDJ.settings')
+
+application = get_asgi_application()
